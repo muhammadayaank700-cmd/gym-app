e4427bc7b968ea14a57fc08373f8ae0cd0fac041
Ayaan K <muhammad.khan-61@student.manchester.ac.uk>
2026-06-12 04:45:25 +0100

    made an html file and served it using flask

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "the gym app is being created :s"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
