abe81bfaf18a0f4c7358359e32cc7bcb4fc51f98
Ayaan K <muhammad.khan-61@student.manchester.ac.uk>
2026-06-19 04:52:19 +0100

    created form and also implemented post requests for adding workouts along with the weights

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' , methods = ["GET" , "POST"])
 def home():
+    if request.method == "POST":
+        workout = request.form.get("workout_name")
+        weight = request.form.get("weight")
+
+        print(f"databse simulation: saving {workout} and the weight {weight}")
+
+
     return render_template("index.html")
 
 if __name__ == "__main__":
