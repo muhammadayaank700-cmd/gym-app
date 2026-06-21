4dfe59baa939421715dfec5a3550cc4f168826dd
Ayaan K <muhammad.khan-61@student.manchester.ac.uk>
2026-06-21 10:29:41 +0100

    used a list because of lack of database , dynamic updating establised using jinja

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask , render_template , request
 
 app = Flask(__name__)
 
+workouts = [] #creating temporary database using list
+
 @app.route('/' , methods = ["GET" , "POST"])
 def home():
     if request.method == "POST":
         workout = request.form.get("workout_name")
         weight = request.form.get("weight")
+        
+        workouts.append({"name":workout , "weight": weight})
 
-        print(f"databse simulation: saving {workout} and the weight {weight}")
-
-
-    return render_template("index.html")
+    return render_template("index.html", savedworkout = workouts)
 
 if __name__ == "__main__":
     app.run(debug=True)
