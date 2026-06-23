edf15d345e061c8603fd93b94d45ac999a62a972
Ayaan K <muhammad.khan-61@student.manchester.ac.uk>
2026-06-23 07:08:19 +0100

    implemented a sqlite database , accounted for injections , cleaned up connections

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
-from flask import Flask , render_template , request
+import sqlite3
+from flask import Flask , render_template , request , redirect
 
 app = Flask(__name__)
 
-workouts = [] #creating temporary database using list
+def init_db():
+    conn = sqlite3.connect("gym.db")
+    cursor = conn.cursor()
 
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS workouts(id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT NOT NULL , weight TEXT NOT NULL)''')
+    conn.commit()
+    conn.close()
+
+init_db()
 @app.route('/' , methods = ["GET" , "POST"])
 def home():
+    
+
+    
+    
     if request.method == "POST":
+        conn = sqlite3.connect("gym.db")
+        cursor = conn.cursor()
         workout = request.form.get("workout_name")
         weight = request.form.get("weight")
-        
-        workouts.append({"name":workout , "weight": weight})
 
-    return render_template("index.html", savedworkout = workouts)
+        cursor.execute("INSERT INTO workouts (name, weight) VALUES (? , ?)",(workout,weight))
+        conn.commit()
+        conn.close()
+        return redirect("/")
 
+    conn = sqlite3.connect("gym.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT name , weight FROM workouts")
+    rows = cursor.fetchall()
+    conn.close()
+
+    formatted_workouts = [{"name":row[0] , "weight":row[1]} for row in rows]
+
+
+    return render_template("index.html", savedworkout = formatted_workouts)
+    
 if __name__ == "__main__":
     app.run(debug=True)
