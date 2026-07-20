5f10daa042c82e26b220a88e9c9731974ac08d6f
Ayaan K <muhammad.khan-61@student.manchester.ac.uk>
2026-07-20 13:07:20 +0100

    made the database relational , used postgres instead of sqlite , update backend to include route for registering users

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-import sqlite3
+import psycopg2
 from flask import Flask , render_template , request , redirect , url_for
 from datetime import date 
 app = Flask(__name__)
 
 def init_db():
-    conn = sqlite3.connect("gym.db")
+    conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
     cursor = conn.cursor()
 
-    cursor.execute(''' CREATE TABLE IF NOT EXISTS workouts(id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT NOT NULL , weight TEXT NOT NULL , date TEXT NOT NULL)''')
+   
+    
+    cursor.execute("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username TEXT UNIQUE NOT NULL , password_hash TEXT NOT NULL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS workout(id SERIAL PRIMARY KEY,name TEXT NOT NULL,weight TEXT NOT NULL,date TEXT NOT NULL, user_id INTEGER REFERENCES users(id))")
     conn.commit()
     conn.close()
 
@@ -19,18 +22,18 @@ def home():
     
     
     if request.method == "POST":
-        conn = sqlite3.connect("gym.db")
+        conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
         cursor = conn.cursor()
         workout = request.form.get("workout_name")
         weight = request.form.get("weight")
         current_date = date.today().strftime("%Y-%m-%d")
 
-        cursor.execute("INSERT INTO workouts (name, weight,date) VALUES (? , ?,?)",(workout,weight,current_date))
+        cursor.execute("INSERT INTO workouts (name, weight,date) VALUES (%s , %s,%s)",(workout,weight,current_date))
         conn.commit()
         conn.close()
         return redirect("/")
 
-    conn = sqlite3.connect("gym.db")
+    conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
     cursor = conn.cursor()
     cursor.execute("SELECT id , name , weight , date FROM workouts")
     rows = cursor.fetchall()
@@ -43,10 +46,10 @@ def home():
 
 @app.route("/delete/<int:workout_id>")
 def delete(workout_id):
-    conn = sqlite3.connect("gym.db")
+    conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
     cursor = conn.cursor()
 
-    cursor.execute("DELETE FROM workouts WHERE id = ?",(workout_id,))
+    cursor.execute("DELETE FROM workouts WHERE id = %s",(workout_id,))
     conn.commit()
     conn.close()
 
@@ -54,7 +57,7 @@ def delete(workout_id):
 
 @app.route('/update/<int:workout_id>/' , methods = ["GET" , "POST"])
 def update(workout_id):
-    conn = sqlite3.connect("gym.db")
+    conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
     cursor = conn.cursor()
 
     if request.method == "POST":
@@ -62,13 +65,13 @@ def update(workout_id):
         new_weight = request.form.get("weight")
         new_date = request.form.get("date")
 
-        cursor.execute("UPDATE workouts SET name = ? , weight = ? , date = ? WHERE id =?",(new_name,new_weight,new_date,workout_id))
+        cursor.execute("UPDATE workouts SET name = %s , weight = %s , date = %s WHERE id = %s",(new_name,new_weight,new_date,workout_id))
         conn.commit()
         conn.close()
 
         return redirect(url_for("home"))
     
-    cursor.execute("SELECT id , name , weight , date FROM workouts WHERE id = ?",(workout_id,))
+    cursor.execute("SELECT id , name , weight , date FROM workouts WHERE id = %s",(workout_id,))
     row = cursor.fetchone()
     conn.close()
 
