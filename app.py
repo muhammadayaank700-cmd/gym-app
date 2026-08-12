c47f949bc962ccce3343f4fe60482cdc434fd9e5
Ayaan K <muhammad.khan-61@student.manchester.ac.uk>
2026-08-12 01:08:51 +0100

    refactor: secure database credentials with environment variables and fix schema

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 from flask import session
 from werkzeug.security import generate_password_hash , check_password_hash
 from flask import Flask , render_template , request , redirect , url_for
 from datetime import date 
+load_dotenv()
+
 app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+DB_URL = os.getenv("DATABASE_URL")
+
+
+
 
 def init_db():
-    conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
+    conn = psycopg2.connect(DB_URL)
     cursor = conn.cursor()
+    cursor.execute("DROP TABLE IF EXISTS workout CASCADE")   # Deletes the old singular table
+    cursor.execute("DROP TABLE IF EXISTS workouts CASCADE")  # Deletes the plural table
+    cursor.execute("DROP TABLE IF EXISTS users CASCADE")
 
    
     
     cursor.execute("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username TEXT UNIQUE NOT NULL , password_hash TEXT NOT NULL)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS workout(id SERIAL PRIMARY KEY,name TEXT NOT NULL,weight TEXT NOT NULL,date TEXT NOT NULL, user_id INTEGER REFERENCES users(id))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS workouts(id SERIAL PRIMARY KEY,name TEXT NOT NULL,weight TEXT NOT NULL,date TEXT NOT NULL, user_id INTEGER REFERENCES users(id))")
     conn.commit()
     conn.close()
 
@@ -20,35 +32,39 @@ init_db()
 @app.route('/' , methods = ["GET" , "POST"])
 def home():
     
+    if "user_id" not in session:
+        return redirect("/login")
 
     
     
     if request.method == "POST":
-        conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
+        conn = psycopg2.connect(DB_URL)
         cursor = conn.cursor()
         workout = request.form.get("workout_name")
         weight = request.form.get("weight")
         current_date = date.today().strftime("%Y-%m-%d")
 
-        cursor.execute("INSERT INTO workouts (name, weight,date) VALUES (%s , %s,%s)",(workout,weight,current_date))
+        user_id = session["user_id"]
+
+        cursor.execute("INSERT INTO workouts (name, weight,date,user_id) VALUES (%s , %s,%s,%s)",(workout,weight,current_date,user_id))
         conn.commit()
         conn.close()
         return redirect("/")
 
-    conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
+    conn = psycopg2.connect(DB_URL)
     cursor = conn.cursor()
-    cursor.execute("SELECT id , name , weight , date FROM workouts")
+    cursor.execute("SELECT id , name , weight , date FROM workouts WHERE user_id = %s",(session["user_id"],))
     rows = cursor.fetchall()
     conn.close()
 
     formatted_workouts = [{"id":row[0] , "name":row[1] , "weight":row[2] , "date":row[3]} for row in rows]
 
 
-    return render_template("index.html", savedworkout = formatted_workouts)
+    return render_template("index.html", savedworkout = formatted_workouts , username = session.get("username"))
 
 @app.route("/delete/<int:workout_id>")
 def delete(workout_id):
-    conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
+    conn = psycopg2.connect(DB_URL)
     cursor = conn.cursor()
 
     cursor.execute("DELETE FROM workouts WHERE id = %s",(workout_id,))
@@ -59,7 +75,7 @@ def delete(workout_id):
 
 @app.route('/update/<int:workout_id>/' , methods = ["GET" , "POST"])
 def update(workout_id):
-    conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
+    conn = psycopg2.connect(DB_URL)
     cursor = conn.cursor()
 
     if request.method == "POST":
@@ -89,7 +105,7 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
+        conn = psycopg2.connect(DB_URL)
         cursor = conn.cursor()
 
         cursor.execute("SELECT id , password_hash FROM users WHERE username = %s",(username,))
@@ -97,7 +113,7 @@ def login():
         
         #if username in database
         if user and check_password_hash(user[1],password):
-            session["user id"] = user[0]
+            session["user_id"] = user[0]
             session["username"]= username
 
             conn.close()
@@ -119,7 +135,7 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        conn = psycopg2.connect("postgresql://neondb_owner:npg_rFMEqJk3gae9@ep-crimson-voice-zai9b3hf.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require")
+        conn = psycopg2.connect(DB_URL)
         cursor = conn.cursor()
 
         cursor.execute("SELECT id FROM users WHERE username = %s",(username,))
@@ -141,6 +157,10 @@ def register():
         #get request
     return render_template("register.html",error=error)
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
 
 
 if __name__ == "__main__":
